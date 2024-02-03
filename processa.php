d38b29f6f59f1dd6c3aca3fd8bdae2b2a2c27669
JezielHonorato <jezielh.ss@gmail.com>
2024-02-03 10:27:17 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -10,15 +10,15 @@
   $ordem       = $_GET['ordem'];
 
   $condicoes = [];
-  $condicoes[] = $pesquisa  ? "(titulo LIKE '%$pesquisa%' OR autor LIKE '%$pesquisa%')" : '';
-  $condicoes[] = $categoria ? "idCategoria = $categoria" : '';
-  $condicoes[] = $pais      ? "idPais = $pais" : '';
+  $pesquisa  ? $condicoes[] = "(titulo LIKE '%$pesquisa%' OR autor LIKE '%$pesquisa%')" : '';
+  $categoria ? $condicoes[] = "idCategoria = $categoria" : '';
+  $pais      ? $condicoes[] = "idPais = $pais" : '';
   $condicoes[] = "data BETWEEN $range_menor AND $range_maior";
 
   $sql_code_livro = "SELECT idLivro, titulo, autor, data FROM tblivro AS l INNER JOIN tbautor AS a ON a.idAutor = l.idAutor";
   $sql_code_livro .= " WHERE " . implode(" AND ", $condicoes);
   $sql_code_livro .= " $ordem";
-  $sql_query_livro = $conexao->query($sql_code_livro) or die($sql_code_livro);
+  $sql_query_livro = $conexao->query($sql_code_livro) or die($conexao->error);
 
   while ($livro = $sql_query_livro->fetch_assoc()) {
     $titulo = $livro['titulo'];
